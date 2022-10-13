8a9d32996c08013f1a3e1486672c1621cfe7325b
jotagaribaldi <joaogaribaldi@gmail.com>
2022-10-13 14:42:00 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth'
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBE3IbpsEVC5YIypN5W3DaSWO80x5sNMvc",
-  authDomain: "topagoapp.firebaseapp.com",
-  projectId:  "topagoapp",
-  storageBucket: "topagoapp.appspot.com",
-  messagingSenderId: "548820232443",
-  appId: "1:548820232443:web:f1e22434bd4555f7eb685e",
-  measurementId: "G-0KF2N4YPMD"
+  apiKey: "A",
+  authDomain: "toom",
+  projectId:  "topp",
+  storageBucket: "topm",
+  messagingSenderId: "53",
+  appId: "1:54",
+  measurementId: "N4YPMD"
 };
 
 // Initialize Firebase and Firebase Authentication
